b52ba91fdb0f1db9a7f9ddf0745cbf4f031e01c0
oligeon-dev <oligeon.dev@gmail.com>
2024-09-06 13:13:42 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -49,13 +49,13 @@ export default defineConfig({
             purpose: "maskable any",
           },
           {
-            src: "192-192.png",
+            src: "192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable any",
           },
           {
-            src: "512-512.png",
+            src: "512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable any",
