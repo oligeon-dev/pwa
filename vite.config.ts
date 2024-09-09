18fff8630219d2db7a09e07f4d930a681177bd0b
oligeon-dev <oligeon.dev@gmail.com>
2024-09-09 17:54:11 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/client/",
-  define: {
-    ...(process.env.IS_STORYBOOK_BUILDING ? {} : { global: "window" }),
-  },
   plugins: [
     react(),
     VitePWA({
