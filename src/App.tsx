a4c14eaf0bc57827dbb1c3c2c130605b3ac868e8
oligeon-dev <oligeon.dev@gmail.com>
2024-09-10 16:23:20 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.container}>
+        <input type="number" />
+        <div className={styles.footer}>footer</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button>version: 0.0.11</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
