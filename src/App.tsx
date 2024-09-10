f7c7db03b1cab1d96e2abc7f9d6294ea4ae06d4e
oligeon-dev <oligeon.dev@gmail.com>
2024-09-10 16:44:18 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { useEffect } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  useEffect(() => {
-    // ビューポートの高さを設定する関数
-    const adjustHeight = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    // 初回ロード時とリサイズ時にビューポートの高さを調整
-    window.addEventListener("resize", adjustHeight);
-    adjustHeight();
-
-    // クリーンアップ: イベントリスナーを削除
-    return () => {
-      window.removeEventListener("resize", adjustHeight);
-    };
-  }, []);
   return (
     <>
-      <div
-        className={styles.container}
-        style={{ height: "calc(var(--vh, 1vh) * 100)" }}
-      >
+      <div className={styles.container}>
+        <p>version1</p>
         <input type="number" />
         <div className={styles.footer}>footer</div>
       </div>
