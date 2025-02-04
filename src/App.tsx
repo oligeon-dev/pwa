33a1ec81c69ef2e903cb871628e1e8d221c252fe
oligeon-dev <oligeon.dev@gmail.com>
2025-02-04 23:18:28 +0900

    open in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './App.css';
 function App() {
   const isOpenInPWA = window.matchMedia('(display-mode: standalone)').matches;
 
+  const openInPWA = () => {
+    window.location.href = `intent://${window.location.host}${window.location.pathname}#Intent;scheme=https;end;`;
+  };
+
   return (
     <div>
       <h1>PWA TEST</h1>
 
       <p>PWAで開いていますか？: {isOpenInPWA ? 'Yes' : 'No'}</p>
+
+      <button onClick={openInPWA}>アプリで開く</button>
     </div>
   );
 }
