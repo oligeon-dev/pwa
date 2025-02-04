b3659f386a19b69d796a50dc2d44dcbc32766af2
oligeon-dev <oligeon.dev@gmail.com>
2025-02-04 23:30:55 +0900

    open app link

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import './App.css';
 function App() {
   const isOpenInPWA = window.matchMedia('(display-mode: standalone)').matches;
 
+  // const openInPWA = () => {
+  //   window.location.href = `intent://${window.location.host}${window.location.pathname}#Intent;scheme=https;end;`;
+  // };
+  // iOSでは動作しない
+  // 「ページを開けません。アドレスが無効です。」のポップアップが表示される
   const openInPWA = () => {
-    window.location.href = `intent://${window.location.host}${window.location.pathname}#Intent;scheme=https;end;`;
+    window.location.replace(
+      `intent://${window.location.host}${window.location.pathname}#Intent;scheme=https;end;`
+    );
   };
 
   return (
@@ -14,6 +21,10 @@ function App() {
       <p>PWAで開いていますか？: {isOpenInPWA ? 'Yes' : 'No'}</p>
 
       <button onClick={openInPWA}>アプリで開く</button>
+
+      <a href='https://main.dsorxvjzdogy0.amplifyapp.com/'>
+        アプリで開く(リンク)
+      </a>
     </div>
   );
 }
