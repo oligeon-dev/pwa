57331eec90f0d26bffa58ef86026acabdcd85367
oligeon-dev <oligeon.dev@gmail.com>
2025-02-04 22:50:37 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
+});
