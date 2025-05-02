5ce533f2d6726ea8c96e5b56686a2b53843be9f6
oligeon-dev <oligeon.dev@gmail.com>
2025-05-02 20:39:43 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const isInstalled = useInstalledRelatedApp();
 
   const [token, setToken] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const [_, setError] = useState<string | null>(null);
 
   useEffect(() => {
     // トークン取得して state にセット
